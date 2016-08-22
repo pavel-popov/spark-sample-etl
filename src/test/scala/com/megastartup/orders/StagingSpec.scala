4ef0f6f2aa28022854eddc55ca0c820c3458d5f3
Pavel Popov <pavelpopov@outlook.com>
2016-08-22 04:20:28 -0700

    Add unit tests for Staging Minodo #1

--- src/test/scala/com/megastartup/orders/StagingSpec.scala
+++ src/test/scala/com/megastartup/orders/StagingSpec.scala
@@ -27,4 +27,25 @@ class StagingSpec extends UnitSpec with SparkSpec {
       Staging("GB123",  0, "2016-05-11", 10, Some(20000.80))))
   }
 
+  "Data from Minodo" should "be staged" in {
+    Given("sample data from Minodo")
+    val input = Array(
+      "et@Riffpath.info,2,1436003685,10,8832.43",
+      "rSanders@Leexo.name,2,1468241985,1,1146.48",
+      "ToddGibson@Topicshots.info,1,1425932126,8,5291.74",
+      "voluptate_vel@Zooveo.org,2,1464874707,2,1216.32",
+      "aliquam_exercitationem@Innotype.org,1,1468685551,8,9267.39"
+    )
+
+    When("staging data")
+    val result = Staging.fromMinodo(sc.parallelize(input)).collect()
+
+    Then("result is staged")
+    result should equal(Array(
+      Staging("et@Riffpath.info",                    64, "2015-07-04", 10, Some(8832.43)),
+      Staging("rSanders@Leexo.name",                 64, "2016-07-11", 1,  Some(1146.48)),
+      Staging("ToddGibson@Topicshots.info",          30, "2015-03-09", 8, Some(5291.74)),
+      Staging("voluptate_vel@Zooveo.org",            64, "2016-06-02", 2, Some(1216.32)),
+      Staging("aliquam_exercitationem@Innotype.org", 30, "2016-07-16", 8, Some(9267.39))))
+  }
 }
