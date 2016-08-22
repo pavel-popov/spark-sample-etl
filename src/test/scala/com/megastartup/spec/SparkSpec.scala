6ca6ef1873de904327cb4256e6581b87c5e8fecd
Pavel Popov <pavelpopov@outlook.com>
2016-08-22 02:21:14 -0700

    Add SparkSpec #1

---
+++ src/test/scala/com/megastartup/spec/SparkSpec.scala
@@ -0,0 +1,36 @@
+// attribution: https://github.com/mkuthan/example-spark
+package com.megastartup.spec
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest._
+
+trait SparkSpec extends BeforeAndAfterAll {
+  this: Suite =>
+
+  private var _sc: SparkContext = _
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName(this.getClass.getSimpleName)
+
+    sparkConfig.foreach { case (k, v) => conf.setIfMissing(k, v) }
+
+    _sc = new SparkContext(conf)
+  }
+
+  def sparkConfig: Map[String, String] = Map.empty
+
+  override def afterAll(): Unit = {
+    if (_sc != null) {
+      _sc.stop()
+      _sc = null
+    }
+    super.afterAll()
+  }
+
+  def sc: SparkContext = _sc
+
+}
